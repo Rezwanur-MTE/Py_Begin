02e169c1b17de24988cefdd42534c28f6aa7901b
Rezwanur Rahman <rezwanurrahman.mte@gmail.com>
2022-02-18 02:09:39 +0600

    This is my 1st code in Python

---
+++ main.py
@@ -0,0 +1,21 @@
+
+'''name=input("Enter His name :")
+age=int(input(" the age :"))
+gpa=float(input("PUT CG :"))
+
+print(" Programmer's name is "+name)
+print("His age is ",age)
+print("CGPA ",gpa)
+sum=age+gpa
+print("Sum of age and GPA is ",sum)
+'''
+from math import *
+
+result= max(30,50,25)
+result2=min(20,90)
+result3=abs(-7)
+result4= sqrt(49)
+result5=pow(2,8)
+result6=round(69.9675564)
+
+print(result, "  ", result2," ",result3," ",result4," ",result5," ",result6)
