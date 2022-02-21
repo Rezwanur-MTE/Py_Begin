2e2081753134a4db68799cd2b5f7b6530cb83570
Rezwanur Rahman <rezwanurrahman.mte@gmail.com>
2022-02-22 01:05:45 +0600

    I changed something, Still in Basic

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ print("His age is ",age)
 print("CGPA ",gpa)
 sum=age+gpa
 print("Sum of age and GPA is ",sum)
-'''
+
 from math import *
 
 result= max(30,50,25)
@@ -31,4 +31,56 @@ print("my id is 1808059")
 result7= 30<20
 print(result7)
 
-print("I dont want to go RUET said by Bashar")
+##Control Statement :
+mark= int(input("Put your marks: "))
+if mark<=33:
+    print("You are failed")
+if mark>=33:
+    print("Congratulation Dude, You passed")
+if mark%2==0:
+    print("Your mark is even")
+else :
+    print("Your mark is Odd")
+
+## use of elif
+
+if mark>=80:
+    print("You Got A+")
+elif mark>=70:
+    print("You Got A")
+elif mark>=60:
+    print("You Got A-")
+elif mark>=50:
+    print("You Got B")
+else:
+    print("I am sorry for you, no Grade achieved")
+
+'''
+
+##Nested if
+if 7>3:  ## if this condition is true then go to nest line
+    if 7>8:
+        print("Hey Hi")
+    else:
+        print("What up boy?")
+num1=20
+num2=30
+num3=40
+if num1>num2:
+    if num1>num3:
+        print("number 1 is bigger")
+    else:
+        print("number 3 is bigger")
+if num2>num1:
+    if num2>num3:
+        print("Number 2 bigger")
+    else :
+        print("Number 3 bigger ")
+
+## logical operator && , ||
+
+num4=73
+if num4>=80 and num4<=100:      ## if one condition is false then doesnt give output
+    print("80 to 100 is A+ Range")
+if 70<=num4<=80:
+    print("70 to 80 is A range")     ## Python can use this too
